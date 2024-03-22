48ba88a4fb3ab0b0489f3c5ca3d236d39f2b6c3a
G M Varun <gmvarun@Gs-MacBook-Air.local>
2024-03-22 09:30:31 +0530

    fix(package):- fix scripts for deployment

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
